9c2c4577b7e4740c3f76d34cd302132cd5c230d4
Noé Cabannes-Michel <63971354+NoeCabannes@users.noreply.github.com>
2025-04-25 06:41:12 +0200

    Update sparkle-animation.js

--- js/sparkle-animation.js
+++ js/sparkle-animation.js
@@ -58,7 +58,7 @@
     let frameRequest; // Stores the requestAnimationFrame ID
 
     // Throttle interval: Minimum time (ms) between sparkle bursts
-    const sparkleIntervalThreshold = 16;
+    const sparkleIntervalThreshold = 25;
     // Distance threshold: Minimum distance (pixels) mouse must move
     const distanceThreshold = 4;
 
